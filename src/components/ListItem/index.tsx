a9c9ac0faad426254b5486c6bb5b33dc46bffd62
Joe Fenix <joefenix.jf@gmail.com>
2022-01-26 21:53:45 -0300

    Lista de tarefas
    projeto base de boniek lacerda

---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import * as S from "./styles";
+import { Item } from "../../types/Item";
+
+type Props = {
+  item: Item;
+}
+export const ListItem = ({ item }: Props) => {
+  const [isChecked, setIsChecked] = useState(item.done);
+  return (
+    <S.Container done={isChecked}>
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={e => setIsChecked(e.target.checked)}
+      />
+      <label>{item.name}</label>
+    </S.Container>
+  )
+}
